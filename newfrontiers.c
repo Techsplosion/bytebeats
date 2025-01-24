566303edfdcdccf3ba1389e859090d6838951473
onepersonintheuniverse <67820127+Techsplosion@users.noreply.github.com>
2025-01-24 11:11:04 +0300

    newfrontiers.c - faster bassline (2x speed)

--- newfrontiers.c
+++ newfrontiers.c
@@ -11,7 +11,7 @@ int main() {
     int seq[] = {1, 2, 3, 4, 5, 4, 3, 2}, bass[] = {0, 2, 0, 2, 3, 2, 0, 2};
     for (int t = 0; t < 600*32768; t++) {
         int m = (t>>17)&1 ? ((t>>16)&1 ? 3*seq[(t>>13)&7]*t/2 : 5*seq[(t>>13)&7]*t/4) : seq[(t>>13)&7]*t;
-        int b = bass[(t>>14)&7]*t/4;
+        int b = bass[(t>>13)&7]*t/4;
         int mw = m&255;
         int bw = b&255;
         putchar((mw+bw)/2);
